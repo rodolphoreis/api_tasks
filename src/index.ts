25cdc55da3a6595cd093ee53ddc23ee0cd2d45d9
Rodolpho Reis <rodolphoreis@live.com>
2024-01-25 19:54:11 +0000

    refactor: use router

--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,11 @@
-import express, { Request, Response } from "express";
-import { findIndexByID } from "./utils";
+import express from "express";
+import { taskRouter } from "./routes/task-routes";
 
-const app = express();
-app.use(express.json());
 const port = process.env.PORT || 3000;
+const app = express();
 
-let tasks: Task[] = [];
-
-app.get("/task", (req, res) => {
-  return res.status(200).json(tasks);
-});
-
-app.post("/task", (req: Request, res: Response) => {
-  const { text } = req.body;
-
-  const task: Task = {
-    id: tasks.length + 1,
-    text: req.body.text,
-    isCompleted: false,
-  };
-  tasks.push(task);
-  return res.status(201).json();
-});
-
-app.put("/task/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { text } = req.body;
-  const intID = parseInt(id);
-
-  const index = findIndexByID(tasks, intID);
-
-  const payload: Task = {
-    id: intID,
-    text,
-    isCompleted: false,
-  };
-  tasks[index] = payload;
-
-  return res.status(200).json(tasks[index]);
-});
-
-app.patch("/task/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  const { text, isCompleted } = req.body;
-  const intID = parseInt(id);
-
-  const index = findIndexByID(tasks, intID);
-
-  const payload: Task = {
-    id: intID || tasks[index].id,
-    text: text || tasks[index].text,
-    isCompleted: isCompleted || tasks[index].isCompleted,
-  };
-  tasks[index] = payload;
-  return res.status(200).json(tasks[index]);
-});
-
-app.delete("/task/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  const intID = parseInt(id);
-
-  const index = findIndexByID(tasks, intID);
-
-  tasks.splice(index, 1);
-  return res.sendStatus(204);
-});
+app.use(express.json());
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
