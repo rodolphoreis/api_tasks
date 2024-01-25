fb57b2711b183aa49fb3ca7ce637e7ad2ea0ee97
Rodolpho Reis <rodolphoreis@live.com>
2024-01-25 19:48:48 +0000

    feat: return 404 if task not found

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,11 @@
+import { Task } from "../types";
+
 export function findIndexByID(tasks: Task[], id: number): number {
   const index = tasks.findIndex((task) => {
     return task.id === id;
   });
+  if (index === -1) {
+    return 404;
+  }
   return index;
 }
